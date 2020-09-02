113d9b5714fd8d3b2db1a6a563712cd7eb2da8ad
Zach DeLong <zdelong4@ku.edu>
2020-09-02 17:41:29 -0500

    added definition for search() in LinkedList.hpp

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -35,7 +35,15 @@ int LinkedList<T>::size() const
 template <typename T>
 bool LinkedList<T>::search(T value) const
 {
-	 
+	Node<T>* temp = m_front;
+	bool isFound = false;
+    while (temp != nullptr){  
+        if (temp->getValue() == value){ 
+            isFound = true;
+		}  
+        temp = temp->getNext();  
+    }  
+    return isFound;  
 }  
 
 
